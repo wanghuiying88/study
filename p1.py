57226a35db502727b40b1a5a95890002370528e5
wanghuiying88 <wanghuiying88@gmail.com>
2020-04-21 16:01:41 -0700

    add some code

--- p1.py
+++ p1.py
@@ -1,3 +1,18 @@
-print("hello world!")
-print("Hello again!")
+fname = input("Enter file name: ")
+if len(fname) < 1 : fname = "mbox-short.txt"
+
+fh = open(fname)
+count = 0
+
+for line in fh:
+    ln = line.lstrip()
+    if(ln == ''):
+    	continue
+    word = ln.split()
+    if word[0].find("From") >= 0 and word[0].find("From:") == -1:
+        print(word[1])
+        count = count + 1
+
+print("There were", count, "lines in the file with From as the first word")
+
 
